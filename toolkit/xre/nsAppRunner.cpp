9329e9e37494f7adbe1ae569da3b25e578810031
KENZ <KENZ.gelsoft@gmail.com>
2024-08-08 05:43:45 +0900

    Don't check if superuse on Haiku
    - like Android code.
    - as Haiku is single user OS currently.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3720,7 +3720,7 @@ class XREMain {
 #endif
 };
 
-#if defined(XP_UNIX) && !defined(ANDROID)
+#if defined(XP_UNIX) && !defined(ANDROID) && !defined(XP_HAIKU)
 static SmprintfPointer FormatUid(uid_t aId) {
   if (const auto pw = getpwuid(aId)) {
     return mozilla::Smprintf("%s", pw->pw_name);
@@ -3769,7 +3769,7 @@ static bool CheckForUserMismatch() {
   }
   return false;
 }
-#else  // !XP_UNIX || ANDROID
+#else  // !XP_UNIX || ANDROID || XP_HAIKU
 static bool CheckForUserMismatch() { return false; }
 #endif
 
